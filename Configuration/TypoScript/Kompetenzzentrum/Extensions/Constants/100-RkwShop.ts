1dd3931fa923334e40c954a951ce872427473f19
Steffen Kroggel <kroggel@rkw.de>
2019-09-27 13:06:09 +0200

    [ADD] Adding configuration for rkw_shop

--- Configuration/TypoScript/Kompetenzzentrum/Extensions/Constants/100-RkwShop.ts
+++ Configuration/TypoScript/Kompetenzzentrum/Extensions/Constants/100-RkwShop.ts
@@ -6,11 +6,25 @@ plugin.tx_rkwshop {
         // cat=plugin.tx_rkwshop//a; type=integer; label=Pid of terms & conditions
     }
 
-    settings {
-        // cat=plugin.tx_rkwshop//a; type=integer; label=Pid of terms & conditions
-        termsPid = 1381
 
-        // cat=plugin.tx_rkwshop//a; type=integer; label=Uid of BE-User if none is set in plugin
-        backendUserIdForMails = 1
-    }
+	settings {
+
+        # cat=plugin.tx_rkwshop/a; type=boolean; label=Include jQuery?
+        includeJQuery = 0
+
+		# cat=plugin.tx_rkwshop//a; type=integer; label=Pid of terms & conditions
+		termsPid = 1381
+
+        # cat=plugin.tx_rkwshop/a; type=boolean; label=Disable admin mails?
+        disableAdminMails = 0
+
+		# cat=plugin.tx_rkwshop//a; type=string; label=Username of BE-User if none is set in plugin
+		fallbackBackendUserForAdminMails = admin
+
+		# cat=plugin.tx_rkwshop//a; type=integer; label=Maximum amount per order item
+		maxOrderItemAmount = 100
+
+		# cat=plugin.tx_rkwshop//a; type=string; label=List of required fields for shipping address
+		requiredFields = gender, firstName, lastName, address, zip, city
+	}
 }
