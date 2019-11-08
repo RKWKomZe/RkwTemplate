4ede06847946d07944da59ba30a4350d0f7faa50
Steffen Kroggel <kroggel@rkw.de>
2019-11-08 11:23:16 +0100

    [FIX] Fixing rootline-fields, flexforms and hooks

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,8 +4,8 @@ if (!defined('TYPO3_MODE')) {
 }
 
 // register hooks for CDN!
-$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['pageIndexing'][] = 'RKW\\RkwTemplate\\Hooks\\ReplaceExtensionPathHook';
-$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-output'][] = 'RKW\\RkwTemplate\\Hooks\\ReplaceExtensionPathHook->hook_contentPostProc';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['pageIndexing'][] = 'RKW\\RkwTemplate\\Hooks\\ReplaceExtensionPathHook';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-output'][] = 'RKW\\RkwTemplate\\Hooks\\ReplaceExtensionPathHook->hook_contentPostProc';
 
 
 ?>
