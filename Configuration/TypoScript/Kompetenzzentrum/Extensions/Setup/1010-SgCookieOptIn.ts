7d6074a90c3183155527ac95393c029d434a6d59
Steffen Kroggel <kroggel@rkw.de>
2020-02-06 14:15:32 +0100

    [FIX] Removing dependency form sg_cookie_optin

--- Configuration/TypoScript/Kompetenzzentrum/Extensions/Setup/1010-SgCookieOptIn.ts
+++
@@ -1,8 +0,0 @@
-plugin.tx_sgcookieoptin{
-
-    persistence {
-        // cat=plugin.tx_r//a; type=string; label=Default storage PID
-        storagePid = 3757
-    }
-}
-
--- ext_localconf.php
+++ ext_localconf.php
@@ -10,18 +10,6 @@ call_user_func(
         $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['pageIndexing'][] = 'RKW\\RkwTemplate\\Hooks\\ReplaceExtensionPathHook';
         $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-output'][] = 'RKW\\RkwTemplate\\Hooks\\ReplaceExtensionPathHook->hook_contentPostProc';
 
-        //=================================================================
-        // Override some classes in sg_cookie_optin
-        //=================================================================
-        /*
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['SGalinski\\SgCookieOptin\\UserFunction\\AddCookieOptinJsAndCss'] = array(
-            'className' => 'RKW\\RkwTemplate\\SGalinski\\UserFunction\\AddCookieOptinJsAndCss'
-        );
-
-        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['SGalinski\\SgCookieOptin\\Hook\\GenerateFilesAfterTcaSave'] = array(
-            'className' => 'RKW\\RkwTemplate\\SGalinski\\Hook\\GenerateFilesAfterTcaSave'
-        );*/
-
     },
     $_EXTKEY
 );
