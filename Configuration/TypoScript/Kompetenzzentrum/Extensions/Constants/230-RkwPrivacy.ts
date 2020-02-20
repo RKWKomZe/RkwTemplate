849af14cf1e7619b89570c8c60409a1854f23978
Steffen Kroggel <kroggel@rkw.de>
2020-02-20 09:55:53 +0100

    [ADD] Adding configuration for rkwPrivacy-Extension

---
+++ Configuration/TypoScript/Kompetenzzentrum/Extensions/Constants/230-RkwPrivacy.ts
@@ -0,0 +1,17 @@
+plugin.tx_rkwprivacy {
+
+	settings {
+
+	    # cat=plugin.tx_rkwprivacy//a; type=int; label=Pid of data protection declaration
+        dataProtectionPid = 1921
+
+        # cat=plugin.tx_rkwprivacy//a; type=int; label=Pid of imprint
+        imprintPid = 409
+
+        sgCookieOptIn {
+
+            # cat=plugin.tx_rkwprivacy//a; type=int; label=Pid to load data from
+			storagePid = 1
+		}
+    }
+}
