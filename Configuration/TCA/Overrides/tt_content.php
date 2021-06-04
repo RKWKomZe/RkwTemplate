6302c695acdfdd803397d3be0c259a32b6d890b4
Christian Dilger <c.dilger@addorange.de>
2021-06-04 18:57:45 +0200

    [CHANGE] readd image to grid elements

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -8,6 +8,29 @@
 // Defaults for grid elements
 //===============================================================
 
+$GLOBALS['TCA']['tt_content']['types']['gridelements_pi1'] = [
+    'showitem' => '
+    	--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
+        --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.header;header,
+        subheader,
+        tx_gridelements_backend_layout,
+        pi_flexform,
+        tx_gridelements_children,
+        --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+        --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.frames;frames,
+        --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.appearanceLinks;appearanceLinks,
+        --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.images,
+        image,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,--palette--;;language,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+        --palette--;;hidden,
+        --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
+        --div--;LLL:EXT:lang/Resources/Private/Language/locallang_tca.xlf:sys_category.tabs.category,
+        categories,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,rowDescription
+	',
+];
+
 $GLOBALS['TCA']['tt_content']['types']['gridelements_pi1']['columnsOverrides'] = [
     'date' => [
         'config' => [
@@ -27,6 +50,12 @@ $GLOBALS['TCA']['tt_content']['types']['gridelements_pi1']['columnsOverrides'] =
     'header' => [
         'config' => [
             'eval' => 'required',
+        ]
+    ],
+    'image' => [
+        'config' => [
+            'minitems' => 0,
+            'maxitems' => 1,
         ]
     ]
 ];
