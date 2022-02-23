54949d2df3f7b3b0ec4b6f74d0def78112bbfecc
Maximilian Faessler <maximilian@faesslerweb.de>
2022-02-23 14:08:27 +0000

    [ADD] image crop option for events detail page

---
+++ Configuration/TCA/Overrides/tx_rkwevents_domain_model_event.php
@@ -0,0 +1,15 @@
+<?php
+
+$GLOBALS['TCA']['tx_rkwevents_domain_model_event']['columns']['header_image']['config']['overrideChildTca']['columns']['crop']['config']['cropVariants'] = [
+
+    'articleDesktop' => [
+        'title' => 'LLL:EXT:rkw_template/Resources/Private/Language/locallang_db.xlf:pages.keyvisual.imageManipulation.articleDesktop',
+        'allowedAspectRatios' => [
+            '1095:590' => [
+                'title' => '1095 x 590',
+                'value' => 1095 / 590
+            ]
+        ]
+    ],
+
+];
